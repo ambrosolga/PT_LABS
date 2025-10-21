bd0da3e697b1459e1cc934edc379404009bd134c
Olga Ambros <ambrosolga55@gmail.com>
2025-10-21 19:17:37 +0300

    Lab 4 completed: QuadraticEquation and Range classes

---
+++ LAB04/OOP_Basics/OOP_Basics/OOP_Basics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+           
+            Range r1 = new Range(1.0,5.0);
+
+
+
+            r1.End = -6;
+
+
+            Range r2 = new Range(-2.0, 3.0);
+            Range r3 = new Range(0.0, 0.0);
+           
+
+            
+            Console.WriteLine("Проверка класса Range");
+
+            r1.PrintRange();
+            Console.WriteLine($"Точка 3.0 внутри? {r1.IsInside(3.0)}"); 
+            Console.WriteLine($"Точка 5.0 внутри? {r1.IsInside(5.0)}"); 
+            Console.WriteLine();
+
+            r2.PrintRange();
+            Console.WriteLine($"Точка -1.0 внутри? {r2.IsInside(-1.0)}"); 
+            Console.WriteLine();
+
+            r3.PrintRange();
+            Console.WriteLine($"Точка 0.0 внутри? {r3.IsInside(0.0)}"); 
+            Console.WriteLine();
+
+            // Создание неверного интервала (начало > конец)
+            //Range bad = new Range(5.0, 1.0); 
+
+            // Изменение границ через свойства
+             //r1.Start = 6.0; 
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ LAB04/OOP_Basics/OOP_Basics/OOP_Basics/Range.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Range
+{
+    private double start;
+    private double end;
+
+    
+    public double Start
+    {
+        get { return start; }
+        set
+        {
+            if (value > end)
+                throw new ArgumentException("Начало интервала не может быть больше его конца.");
+            start = value;
+        }
+    }
+
+   
+    public double End
+    {
+        get { return end; }
+        set
+        {
+            if (value < start)
+                throw new ArgumentException("Конец интервала не может быть меньше его начала.");
+            end = value;
+        }
+    }
+
+    public Range(double start, double end)
+    {
+        if (start > end)
+            throw new ArgumentException("Начало интервала не может быть больше его конца.");
+
+        this.start = start;
+        this.end = end;
+    }
+
+    
+    public bool IsInside(double x)
+    {
+        return x >= start && x < end;
+    }
+
+    
+    public void PrintRange()
+    {
+        Console.WriteLine($"[{start}, {end})");
+    }
+}
