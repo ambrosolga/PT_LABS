10ddfd96926023ed098df0bbff09256602c60c4e
Olga Ambros <ambrosolga55@gmail.com>
2025-11-16 20:28:17 +0300

    лаб работа №5

---
+++ LAB05/TASK_0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP_RATIONAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Rational r1 = new Rational(4, 8);
+            Rational r2 = new Rational(2, -5);
+            Rational r3 = new Rational(-3, -4);
+
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+            Console.WriteLine(r3);
+
+            Console.WriteLine($"r1 == r2: {r1 == r2}"); 
+            Console.WriteLine($"r1 != r2: {r1 != r2}"); 
+            Console.WriteLine($"r1 > r2: {r1 > r2}");   
+            Console.WriteLine($"r1 < r2: {r1 < r2}");   
+            Console.WriteLine($"r1 >= r2: {r1 >= r2}"); 
+            Console.WriteLine($"r1 <= r2: {r1 <= r2}"); 
+            Console.WriteLine($"r2 == r3: {r2 == r3}"); 
+            Console.WriteLine($"r2 < r3: {r2 < r3}");   
+            Console.WriteLine($"r2 <= r3: {r2 <= r3}");
+        }
+    }
+}
